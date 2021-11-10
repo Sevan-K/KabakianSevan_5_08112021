677064b2064b8f0b63cb0b6a484d0702b941c704
Sevan-K <sevan.kabakian@gmail.com>
2021-11-10 12:39:36 +0100

    Products adding complete

--- front/js/index.js
+++ front/js/index.js
@@ -1,11 +1,43 @@
-// function to obtain all the products
+/* ----------------------------------------- */
+/*       Adding products to index page       */
+/* ----------------------------------------- */
+
+// selection of section with id items
+const items = document.getElementById("items");
+
+// function to create a product
+function addProduct(products, index) {
+  // product card HTML elements creation
+  const ancre = document.createElement("a");
+  const article = document.createElement("article");
+  const image = document.createElement("img");
+  const productName = document.createElement("h3");
+  const productDescription = document.createElement("p");
+  // adding needed classes
+  productName.classList.add("productName");
+  productDescription.classList.add("productDescription");
+  // construction de la structure html
+  items.appendChild(ancre);
+  ancre.appendChild(article);
+  article.appendChild(image);
+  article.appendChild(productName);
+  article.appendChild(productDescription);
+  // ading content to the HTML elements
+  productName.textContent = products[index].name;
+  productDescription.textContent = products[index].description;
+  image.setAttribute("src", products[index].imageUrl);
+  image.setAttribute("alt", products[index].altTxt);
+}
+// function to obtain all the products datas
 let getProducts = async function () {
   try {
     let response = await fetch("http://localhost:3000/api/products");
     if (response.ok) {
       let data = await response.json();
-      console.log(data);
-      return data;
+      for (let i in data) {
+        console.log(data[i]);
+        addProduct(data, i);
+      }
     } else {
       console.log(response.status);
     }
@@ -13,21 +45,5 @@ let getProducts = async function () {
     console.log(error);
   }
 };
+// calling the function to get products data and add them
 getProducts();
-
-// product card HTML elements creation
-const items = document.getElementById("items");
-const ancre = document.createElement("a");
-const article = document.createElement("article");
-const image = document.createElement("img");
-const productName = document.createElement("h3");
-const productDescription = document.createElement("p");
-// adding needed classes
-productName.classList.add("productName");
-productDescription.classList.add("productDescription");
-// construction de la structure html
-items.appendChild(ancre);
-ancre.appendChild(article);
-article.appendChild(image);
-article.appendChild(productName);
-article.appendChild(productDescription);
