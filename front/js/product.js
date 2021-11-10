56150ef99ec9238890ed9904e563342b2bade512
Sevan-K <sevan.kabakian@gmail.com>
2021-11-10 19:48:14 +0100

    Récupération de l'url de la page

---
+++ front/js/product.js
@@ -0,0 +1,8 @@
+
+/* ---------------------------------------------------- */
+/*          Adding product's data to the  page          */
+/* ---------------------------------------------------- */
+
+// let urlContent = window.location.href
+console.log(window.location.href);
+// utiliser l'API avec un /et l'id du produit pour faire la sélection
