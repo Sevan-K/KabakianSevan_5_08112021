2a8057c4c9a309dc353296d4a9971bef6cabb882
Sevan-K <sevan.kabakian@gmail.com>
2021-11-12 17:51:36 +0100

    Ajout des éléments du produit dans le DOM

--- front/js/product.js
+++ front/js/product.js
@@ -31,6 +31,20 @@ let getProductById = async function () {
 const intemImg = document.getElementsByClassName("item__img")[0];
 const image = document.createElement("img");
 intemImg.appendChild(image);
+const title = document.getElementById("title");
+const price = document.getElementById("price");
+const description = document.getElementById("description");
+const colorSelect = document.getElementById("colors");
+
+// function to add the different colors into the select tag
+let addColors = (colors) => {
+  for (let i = 0; i < colors.length; i++) {
+    let color = document.createElement("option");
+    colorSelect.appendChild(color);
+    color.setAttribute("value", colors[i]);
+    color.textContent = colors[i];
+  }
+};
 
 // function to add product info
 async function addProductInfo() {
@@ -40,5 +54,9 @@ async function addProductInfo() {
   // adding product info into HTML elmeents
   image.setAttribute("src", product.imageUrl);
   image.setAttribute("alt", product.altTxt);
+  title.textContent = product.name;
+  price.textContent = " " + product.price + " ";
+  description.textContent = product.description;
+  addColors(product.colors);
 }
 addProductInfo();
