39ccc2dcdbd389852842248b2a31948822381c8f
Sevan-K <sevan.kabakian@gmail.com>
2021-11-17 12:59:14 +0100

    Ajjout de la fonction pour savoir si un article est dans le panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -15,21 +15,38 @@ console.log("Test stringified : ", testStringified);
 // localStorage.setItem("testId", test.id);
 // localStorage.setItem("testQuantity", test.quantity);
 // localStorage.setItem("testColor", test.color);
-localStorage.setItem(test.id + "_" + test.color, testStringified);
-
+let testKey = test.id + "_" + test.color;
+localStorage.setItem(testKey, testStringified);
 // const testRecovered = new CartItem(localStorage.getItem("testId"),localStorage.getItem("testQuantity"),localStorage.getItem("testColor"));
-testRecovered = JSON.parse(localStorage.getItem("test"));
+testRecovered = JSON.parse(localStorage.getItem(testKey));
 console.log("Test récupéré du local storage et parsé : ", testRecovered);
-
 console.log(localStorage);
 // localStorage.clear()
 // console.log(localStorage);
 
 // faire une fonction getItemToAddToCart qui construit la valeur de l'item à ajouter au panier
 
+// fonction qui parcours le panier et précise si l'article (id + couleur) est déjà présent
+let getItemInfoOnCart = (cart, item) => {
+  let itemInfoOnCart = {
+    cartIndex: 0,
+    isIdOnCart: false,
+    isItemOnCart: false,
+  };
+  for (let i in cart) {
+    if (cart[i].id === item.id && cart[i].color === item.color) {
+      itemInfoOnCart.cartIndex = i;
+      itemInfoOnCart.isIdOnCart = true;
+      itemInfoOnCart.isItemOnCart = true;
+    } else if (cart[i].id === item.id) {
+      itemInfoOnCart.isIdOnCart = true;
+      itemInfoOnCart.cartIndex = i;
+    }
+  }
+  console.log("Info de l'article dans le panier", itemInfoOnCart);
+  return itemInfoOnCart;
+};
 // fonction addItemToCart qui prend un newCartItem et l'ajoute au panier si nécessaire
-
-// ces deux fonction seront appelées lorsqu'on clique sur le bonton "ajouter au panier"
 
 // fonction qui construit le panier à partir du local storage
 function buildCartFromStorage() {
@@ -39,7 +56,15 @@ function buildCartFromStorage() {
     let value = localStorage.getItem(key);
     cart.push(JSON.parse(value));
   }
-  console.log("Panier construi à partir du localStorage", cart);
+  console.log("Panier construit à partir du localStorage", cart);
   return cart;
 }
+let cart = [];
+getItemInfoOnCart(cart, test);
 
+// fonction pour ajouter le panier au local storage
+
+// ces fonctions seront appelées lorsqu'on clique sur le bonton "ajouter au panier"
+// test si panier vide si oui ajout de l'article
+// sinon appel la fonction pour construire le panier
+// appel de la fonction qui dit
